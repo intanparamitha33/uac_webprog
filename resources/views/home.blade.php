8e35161649c91e60694525f91445062eda54c23a
intanparamitha33 <intanparamitha33@gmail.com>
2023-08-01 11:51:12 +0700

    perbaiki home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,43 @@
 @extends('layout.master')
 @section('title', 'Home')
 @section('content')
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container">
+    <div class="row row-cols-3">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="People's Photo">
+            <div class="card-body">
+              <h5 class="card-title">Jeffrey Nichol</h5>
+              <p class="card-text">Cyber Security</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="People's Photo">
+            <div class="card-body">
+              <h5 class="card-title">Mozart</h5>
+              <p class="card-text">Music Specialist</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="People's Photo">
+            <div class="card-body">
+              <h5 class="card-title">Albert Einstein</h5>
+              <p class="card-text">Scientist</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="People's Photo">
+            <div class="card-body">
+              <h5 class="card-title">Yoasobi</h5>
+              <p class="card-text">Singer</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="People's Photo">
+            <div class="card-body">
+              <h5 class="card-title">BJ Habibie</h5>
+              <p class="card-text">Engineer</p>
+            </div>
+        </div>
     </div>
-</div>
+  </div>
 @endsection
